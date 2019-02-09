945c604613967104f07fd83f4ee2b6ffa0704981
Alopex6414 <1029535012@qq.com>
2019-02-09 17:13:05 +0800

    LiveProject-LiveReStart. add code.

--- LiveReStart/WinMain.cpp
+++ LiveReStart/WinMain.cpp
@@ -24,7 +24,45 @@ int WINAPI WinMain(IN HINSTANCE hInstance, IN HINSTANCE hPrevInstance, IN LPSTR 
 {
 	int nResult = 0;
 
+	// start restart process...
+	CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "ReStart Process: %s", lpCmdLine);
 
+	// check commond lines...
+	nResult = strcmp(lpCmdLine, "");
+	if (nResult == 0)
+	{
+		// no commond lines...(exit)[1]...
+		CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "Process Exception Exit! Return Value=1, No CmdLine Parameter!");
+		return 1;
+	}
+
+	// check process exist...
+	while (true)
+	{
+		if (!CPlumProcess::PlumProcessIsProcessExistExA(lpCmdLine))
+		{
+			CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "Check Process Status: Process Exit.");
+			Sleep(100);
+			break;
+		}
+
+		CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "Check Process Status: Process Exist.");
+		Sleep(100);
+	}
+
+	// restart process...
+	BOOL bResult;
+
+	bResult = CPlumProcess::PlumProcessStartProcessExA(lpCmdLine);
+	if (!bResult)
+	{
+		// can not restart process!...(exit)[2]...
+		CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "Process Exception Exit! Return Value=2, Can not ReStart Process!");
+		return 2;
+	}
+
+	// restart process normal exit...(exit)[0]...
+	CLiveReStartLog::LiveReStartLogExWriteLine(__FILE__, __LINE__, "Process Exit! Return Value=0.");
 
 	return 0;
 }
