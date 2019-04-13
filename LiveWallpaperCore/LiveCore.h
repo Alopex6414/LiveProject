b7711ab223afa57de63688397e1a61323aac74e4
Alopex6414 <1029535012@qq.com>
2019-04-13 23:30:06 +0800

    LiveProject-LiveWallpaperCore. add common header file.

--- LiveWallpaperCore/LiveCore.h
+++ LiveWallpaperCore/LiveCore.h
@@ -1,1 +1,23 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2019, alopex
+*     All rights reserved.
+*
+* @file		LiveCore.h
+* @brief	This Program is LiveWallpaperCore Project.
+* @author	alopex
+* @version	v1.00a
+* @date		2019-04-13
+*/
 #pragma once
+
+#ifndef __LIVECORE_H_
+#define __LIVECORE_H_
+
+// Include Common Header File
+#include "Common.h"
+
+// Include LiveCore Header File
+#include "LiveCoreLog.h"
+
+#endif // !__LIVECORE_H_
