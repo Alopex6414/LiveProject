3fbcb2789299ac3cfde6ea420a9e415865515879
Alopex6414 <1029535012@qq.com>
2019-06-25 22:28:57 +0800

    LiveProject-LiveGraphics update livegraphics.

--- LiveGraphicsCore/LiveGraphics.h
+++ LiveGraphicsCore/LiveGraphics.h
@@ -1,1 +1,17 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2019, alopex
+*     All rights reserved.
+*
+* @file		LiveGraphics.h
+* @brief	This Program is LiveGraphicsCore Project.
+* @author	alopex
+* @version	v1.00a
+* @date		2019-06-22
+*/
 #pragma once
+
+#ifndef __LIVEGRAPHICS_H_
+#define __LIVEGRAPHICS_H_
+
+#endif // !__LIVEGRAPHICS_H_
